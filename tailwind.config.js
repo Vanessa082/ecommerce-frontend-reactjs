0a5636e217074f8351e81e9342669ae60f792f2b
vanessa082 <wahvanessa22@gmail.com>
2024-10-18 19:18:31 +0100

    build the compleate header section of the website template used the map method to create a list in the nav and also for responsiveness use state management to manage state and toggling of the modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: [
+    "./src/**/*.{ts,tsx}"
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#FA4A0C",
+          "secondary": "#ffaa00",
+          "accent": "#252B42",
+        },
+      },
+    ],
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        app: {
+          orange: {
+            900: "#FA4A0C"
+          },
+          blue: {
+            900: "#252B42"
+          },
+          grey: {
+            900: "#737373"
+          }
+        }
+      },
+      fontSize: {
+        xl: '1rem'
+      }
+    },
+  },
+  plugins: [
+    daisyui
+  ],
 }
-
